08115dc63cd9333059b7dd2239528b2906d7f76f
Logan Seaburg <loganseaburg@Logans-MacBook-Air.local>
2024-10-22 21:00:25 -0400

    Merge in docker additions from Ron, plus a few additions

---
+++ docker/start.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+cd /runner
+
+./config.sh --url https://github.com/LSeaburg/zelda3 --token AELHEOWUH3LXEXHZ5ZYQ3ZLHDBHE6
+
+./run.sh
---
+++ docker_build.sh
@@ -0,0 +1,2 @@
+docker build -t zelda3_git_runner -f docker/Dockerfile .
+docker run -it -l z3_runner:latest zelda3_git_runner
